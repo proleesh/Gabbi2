8ed7a8f258b4a9cd3c2237de7b3f34f5e47ff55f
proleesh <merci726@yahoo.com>
2024-09-17 09:38:50 +0900

    update about bean test

--- src/main/java/org/proleesh/component/BeanComponent.java
+++ src/main/java/org/proleesh/component/BeanComponent.java
@@ -1,4 +1,19 @@
 package org.proleesh.component;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BeanComponent {
+
+    @Bean
+    public String name(){
+        return new String("Proleesh");
+    }
+
+//    @Bean(name = "p2", autowireCandidate = false)
+    @Bean(name = "p2", autowireCandidate = true)
+    public String name2(){
+        return new String("Proleesh2");
+    }
 }
