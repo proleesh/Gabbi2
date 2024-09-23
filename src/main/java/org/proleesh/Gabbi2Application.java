333fc1b8aa1b9cd4ffeaf36dd5dbe6e343be8c08
proleesh <merci726@yahoo.com>
2024-09-23 14:11:02 +0900

    update about Configuration

--- src/main/java/org/proleesh/Gabbi2Application.java
+++ src/main/java/org/proleesh/Gabbi2Application.java
@@ -1,13 +1,23 @@
 package org.proleesh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Gabbi2Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Gabbi2Application.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Gabbi2Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Gabbi2Application.class, args);
+        Environment env = run.getBean(Environment.class);
+        log.info("Configured server servlet session timeout value: "
+                + env.getProperty("server.servlet.session.timeout"));
+
     }
 
 }
